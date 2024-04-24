c3601a7946163fe6695c0bca958e432129786aa8
Mihir Yogi <mihiryogif@gmail.com>
2024-04-25 01:15:45 +0530

    todo list is complited

--- JavaScript Tasks/Task 1 (To-Do list )/main.js
+++ JavaScript Tasks/Task 1 (To-Do list )/main.js
@@ -1,19 +1,40 @@
-// for dark and light modes 
+const inputBox = document.getElementById("add-box");
+const listContainer = document.getElementById("list-container");
 
-const body = document.querySelector("body"),
-    sidebar = body.querySelector("#sidebar"),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    mode = body.querySelector(".mode"),
-    modeText = body.querySelector(".mode-text"),
-    modeSwitch = body.querySelector(".toggle-btn");
-    
+const addTask = () => {
+    if(inputBox.value === ''){
+        alert("You must write something!")
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+
+    saveData();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
 
 
-    modeSwitch.addEventListener("click", () => {
-        body.classList.toggle("dark");
-    })
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
 
-    toggle.addEventListener("click", () => {
-        sidebar.classList.toggle("close");
-    })
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
