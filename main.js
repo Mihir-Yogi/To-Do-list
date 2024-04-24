1f83ca28c74cdda88b7a7a852fa7b45e6d7b245c
Mihir Yogi <mihiryogif@gmail.com>
2024-04-25 02:46:52 +0530

    -A

--- main.js
+++ main.js
@@ -2,21 +2,35 @@ const inputBox = document.getElementById("add-box");
 const listContainer = document.getElementById("list-container");
 
 const addTask = () => {
-    if(inputBox.value === ''){
-        alert("You must write something!")
+    if(inputBox.value === ""){
+        alert("You must write something!");
     }else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
+
     inputBox.value = "";
 
     saveData();
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
